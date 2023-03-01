8c98807a08f17823dc15f2abb52f65da4a9ae966
EGL1510M18as <emagonzalez711@gmail.com>
2023-03-01 10:24:12 -0600

    Cuestionarios
    c:

---
+++ uliya-html/js/questionsTablas.js
@@ -0,0 +1,94 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "La etiqueta < a > define un hipervínculo  y su atributo especifica la URL de la página",
+    answer: "href",
+    options: [
+        "href",
+        "style",
+        "src",
+        "title"
+    ]
+    },
+    {
+    numb: 2,
+    question: "La etiqueta < img > se usa para incrustar un imagen en una página HTML y su atributo especifica la ruta a la imagen",
+    answer: "src",
+    options: [
+        "href",
+        "style",
+        "src",
+        "title"
+    ]
+    },
+    {
+    numb: 3,
+    question: "Los <img> la etiqueta también debe contener el width y height atributos, que especifican el ancho y altura de la imagen",
+    answer: "width y height",
+    options: [
+        "href",
+        "width y height",
+        "src",
+        "title"
+    ]
+    },
+    {
+    numb: 4,
+    question: "Este atributo se usa para agregar estilos a un elemento, como color, fuente, tamaño y más.",
+    answer: "style",
+    options: [
+        "href",
+        "width y height",
+        "src",
+        "style"
+    ]
+    },
+    {
+    numb: 5,
+    question: "La etiqueta < html > sirve declarar el lenguaje de la página web. Esto está destinado a ayudar a los motores de búsqueda y navegadores.",
+    answer: "lang",
+    options: [
+        "href",
+        "width y height",
+        "src",
+        "lang"
+    ]
+    },
+    {
+    numb: 6,
+    question: "Este atributo define algún extra de información sobre un elemento (Titulo)",
+    answer: "title",
+    options: [
+        "title",
+        "width y height",
+        "src",
+        "lang"
+    ]
+    },  
+    {
+    numb: 7,
+    question: "La etiqueta < img > tiene un atributo que proporciona un texto alternativo para una imagen",
+    answer: "alt",
+    options: [
+        "title",
+        "alt",
+        "src",
+        "lang"
+    ]
+    }, 
+  // puede descomentar los códigos a continuación y duplicarlos tanto como desee agregar una pregunta
+  // pero recuerde que debe dar el valor adormecido serializar como 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Tu pregunta está aquí",
+  //   answer: "La respuesta correcta de la pregunta está aquí.",
+  //   options: [
+  //     "Opción 1",
+  //     "Opción 2",
+  //     "Opción 3",
+  //     "Opción 4"
+  //   ]
+  // },
+];
